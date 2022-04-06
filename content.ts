6f261ec2043a7fb58fd177ab306dd17561cc9423
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2022-04-06 12:12:59 +0000

    add code in files types: .ts,.json remove code in files types: .tsx,.ts,,.json

--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
 console.log("ffage")
 console.log("gxthv")
 console.log("vhxnl")
-console.log("uurku")
+console.log("qrcmf")
--- content.tsx
+++ content.tsx
@@ -1,3 +1,2 @@
 console.log("hqxbu")
 console.log("oplnq")
-console.log("hmseg")
